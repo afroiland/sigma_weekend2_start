986f14a2e2383536302c3f1c941509de40f89ab1
Andrew Froiland <afroiland@Andrews-MacBook-Pro.local>
2016-11-04 20:58:31 -0500

    Basic functionality

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,54 @@
+
+var studentIndex = 0;
+
 $(document).ready(function(){
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
         console.log(data);
+        displayInfo(data);
+        buttonFunctions(data);
+//        setInterval(automate(data), 2000);
+        }
+      });
+
+    function displayInfo(index){
+      $('#container').append('<div class="person"></div>');
+      var $el = $('#container').children().last();
+      $el.append('<h3>' + index.sigmanauts[studentIndex].name + '</h3>');
+      $el.append('<h3>' + index.sigmanauts[studentIndex].git_username + '</h3>');
+      $el.append('<h3>' + index.sigmanauts[studentIndex].shoutout + '</h3>');
+      $("#td"+studentIndex).css('background', '#aaa');
+    }
+
+    function buttonFunctions(test){
+      $("#next").on('click', function(event) {
+        studentIndex++;
+        if(studentIndex === 19){
+          studentIndex = 0;
+        }
+        $("#container").empty();
+        $("td").css('background', 'none');
+        displayInfo(test);
+      });
+      $("#previous").on('click', function(event) {
+        studentIndex--;
+        if(studentIndex === -1){
+          studentIndex = 18;
+        }
+        $("#container").empty();
+        $("td").css('background', 'none');
+        displayInfo(test);
+      });
+    }
+    function automate(test){
+      studentIndex++;
+      if(studentIndex === 19){
+        studentIndex = 0;
       }
-    });
+      $("#container").empty();
+      $("td").css('background', 'none');
+      displayInfo(test);
+    }
 });
